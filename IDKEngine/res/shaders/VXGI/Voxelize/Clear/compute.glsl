ce518e1c2988e44e361ab0492b8bcee521bd70b0
Julian <88141582+BoyBaykiller@users.noreply.github.com>
2023-09-08 23:12:00 +0200

    fix wrong feature check

--- IDKEngine/res/shaders/VXGI/Voxelize/Clear/compute.glsl
+++ IDKEngine/res/shaders/VXGI/Voxelize/Clear/compute.glsl
@@ -2,7 +2,7 @@
 
 #define TAKE_ATOMIC_FP16_PATH AppInsert(TAKE_ATOMIC_FP16_PATH)
 
-#if TAKE_VERTEX_LAYERED_RENDERING_PATH
+#if TAKE_ATOMIC_FP16_PATH
     #extension GL_NV_shader_atomic_fp16_vector : require
 #endif
 
@@ -10,7 +10,7 @@ layout(local_size_x = 4, local_size_y = 4, local_size_z = 4) in;
 
 layout(binding = 0, rgba16f) restrict uniform image3D ImgResult;
 
-#if !TAKE_VERTEX_LAYERED_RENDERING_PATH
+#if !TAKE_ATOMIC_FP16_PATH
 layout(binding = 1) restrict writeonly uniform uimage3D ImgResultR;
 layout(binding = 2) restrict writeonly uniform uimage3D ImgResultG;
 layout(binding = 3) restrict writeonly uniform uimage3D ImgResultB;
@@ -20,7 +20,7 @@ void main()
 {
     ivec3 imgCoord = ivec3(gl_GlobalInvocationID);
 
-#if TAKE_VERTEX_LAYERED_RENDERING_PATH
+#if TAKE_ATOMIC_FP16_PATH
 
     imageStore(ImgResult, imgCoord, vec4(0.0));
 
